e3b3a87154fdddb5b5f915f4cc2b00f155cc2af3
Vivien-Shah <hfsthane7787@gmail.com>
2025-06-27 19:04:47 +0530

    Add files via upload

---
+++ c10_loops.py
@@ -0,0 +1,35 @@
+'''for i in range (50):
+    print (i)
+name="Computer"
+for i in name : 
+    print (i)'''
+'''Sum of whole numbers
+Outline:
+Write a program to calculate the sum of whole numbers'''
+'''num=int(input("Enter the number whose sun you want to find"))
+sum=0
+for i in range(1,num+1):
+    sum=sum+i
+    print (sum)'''
+
+
+'''Reverse a String
+Outline:
+Write a program to reverse the string entered by the user.
+name=input("Enter any word: ")
+reverse=("")
+for i in name : 
+    reverse= i + reverse
+    
+print (reverse)'''
+
+
+'''reverse order
+Outline:
+Write a program to print the numbers in reverse order beginning 
+from the number entered by the user.'''
+'''num=int(input("Enter the number: "))
+for i in range(num,0,-1): 
+    print(i)'''
+
+
